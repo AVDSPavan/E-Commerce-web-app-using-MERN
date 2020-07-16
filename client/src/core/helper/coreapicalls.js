0f253c72f415b26d07cbe106ad5268365d1b7113
AVDSPavan <saipavan.ala@gmail.com>
2020-07-16 19:39:58 +0530

    solving get products error

--- client/src/core/helper/coreapicalls.js
+++ client/src/core/helper/coreapicalls.js
@@ -1,9 +1,9 @@
 import { API } from "../../backend";
 
 export const getProducts = () => {
-  return fetch(`${API}/products`, { method: "GET" })
-    .then(response => {
-      return response.json();
-    })
-    .catch(err => console.log(err));
+	return fetch(`${API}/products`)
+		.then((response) => {
+			return response.json();
+		})
+		.catch((err) => console.log(err));
 };
